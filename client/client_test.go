e56fd27268e0046c23f8028225560f6a849a4efd
Kat Joyce <katjoyce@google.com>
2018-08-14 11:11:06 +0100

    Add slightly hacky test for Get.

--- client/client_test.go
+++ client/client_test.go
@@ -1,7 +1,13 @@
 package client
 
 import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
 	"testing"
+
+	ct "github.com/google/certificate-transparency-go"
 )
 
 func TestBuildURL(t *testing.T) {
@@ -74,6 +80,59 @@ func TestBuildURL(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			if got := BuildURL(test.baseURL, test.path, test.params); got != test.want {
 				t.Fatalf("BuildURL(%q, %q, %v) = %q, want %q", test.baseURL, test.path, test.params, got, test.want)
+			}
+		})
+	}
+}
+
+func fakeServer(statusCode int, body []byte) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(statusCode)
+		w.Write(body)
+	}))
+}
+
+// TODO(katjoyce): Improve this test - it's currently very lightweight.  Try to
+// find a way to test for all error types that could be returned by Get.
+func TestGet(t *testing.T) {
+	tests := []struct {
+		name        string
+		url         string
+		statusCode  int
+		body        []byte
+		wantErrType reflect.Type
+	}{
+		{
+			name:        "get error",
+			url:         "not-a-real-url",
+			wantErrType: reflect.TypeOf(&GetError{}),
+		},
+		{
+			name:        "HTTP status error",
+			statusCode:  http.StatusNotFound,
+			wantErrType: reflect.TypeOf(&HTTPStatusError{}),
+		},
+		{
+			name:       "no error",
+			statusCode: http.StatusOK,
+			body:       []byte("{\"tree_size\":344104340,\"timestamp\":1534165797863,\"sha256_root_hash\":\"ygEuQj0whDc1GYzvyAFYMKODrZac2Lu3HOnILxJxIqU=\",\"tree_head_signature\":\"BAMARjBEAiBNI3ZY018rZ0/mGRyadQpDrO7lnAA2zRTuGNBp4YJV7QIgD6gWqMf3nqxxcl6K4Rg6sFi+FClVL2S8sbN3JhfCAs8=\"}"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			s := fakeServer(test.statusCode, test.body)
+			lc := New(s.URL, &http.Client{})
+			if test.url != "" {
+				lc = New(test.url, &http.Client{})
+			}
+
+			got, gotErr := lc.Get(ct.GetSTHPath, nil)
+			if gotErrType := reflect.TypeOf(gotErr); gotErrType != test.wantErrType {
+				t.Errorf("Get(_, _): error was of type %s, want %s", gotErrType, test.wantErrType)
+			}
+			if !bytes.Equal(got.Body, test.body) {
+				t.Errorf("Get(_, _): HTTPData.Body = %s, want %s", got.Body, test.body)
 			}
 		})
 	}
