808cafdcbcee5030fce5b4f2ee5acddfef4f0a2b
Kat Joyce <katjoyce@google.com>
2018-09-12 14:50:59 +0100

    Example commit

--- sthgetter/sthgetter.go
+++ sthgetter/sthgetter.go
@@ -4,6 +4,7 @@ package sthgetter
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
@@ -18,7 +19,7 @@ var logStr = "STH Getter"
 // Run runs an STH Getter, which periodically gets an STH from a Log, checks
 // that each one meets per-STH requirements defined in RFC 6962, and stores
 // them.
-func Run(ctx context.Context, lc *client.LogClient, st storage.APICallWriter, url string, period time.Duration) {
+func Run(ctx context.Context, lc *client.LogClient, sv *ct.SignatureVerifier, st storage.APICallWriter, url string, period time.Duration) {
 	log.Printf("%s: %s: started with period %v", url, logStr, period)
 
 	t := time.NewTicker(period)
@@ -27,19 +28,18 @@ func Run(ctx context.Context, lc *client.LogClient, st storage.APICallWriter, ur
 		select {
 		case <-t.C:
 			// TODO(katjoyce): Work out when and where to add context timeouts.
-			getCheckStoreSTH(ctx, url, lc, st)
+			getCheckStoreSTH(ctx, url, lc, sv, st)
 		case <-ctx.Done():
 			log.Printf("%s: %s: stopped", url, logStr)
 			return
 		}
-
 	}
 }
 
-func getCheckStoreSTH(ctx context.Context, url string, lc *client.LogClient, st storage.APICallWriter) {
+func getCheckStoreSTH(ctx context.Context, url string, lc *client.LogClient, sv *ct.SignatureVerifier, st storage.APICallWriter) {
 	// Get STH from Log.
 	log.Printf("%s: %s: getting STH...", url, logStr)
-	_, httpData, getErr := lc.GetSTH()
+	sth, httpData, getErr := lc.GetSTH()
 	if getErr != nil {
 		log.Printf("%s: %s: error getting STH: %s", url, logStr, getErr)
 	}
@@ -54,7 +54,26 @@ func getCheckStoreSTH(ctx context.Context, url string, lc *client.LogClient, st 
 		log.Printf("%s: %s: error writing API Call %s: %s", url, logStr, apiCall, err)
 	}
 
+	if sth == nil {
+		return
+	}
+
 	//TODO(katjoyce): Run checks on the received STH.
+	// Check that the STH signature verifies.
+	log.Printf("%s: %s: verifying signature...", url, logStr)
+	var verificationErr error
+	if err := sv.VerifySTHSignature(*sth); err != nil {
+		verificationErr = &SignatureVerificationError{Err: err}
+	}
+	fmt.Println(verificationErr)
 
 	//TODO(katjoyce): Store STH.
+}
+
+type SignatureVerificationError struct {
+	Err error
+}
+
+func (e *SignatureVerificationError) Error() string {
+	return e.Err.Error()
 }
