b8497a1eb13f783299398148925da2107f7e30b1
Kat Joyce <katjoyce@google.com>
2018-12-06 17:34:39 +0000

    Remove LogID because Pierre says YAGNI. (#13)

--- ctlog/ctlog.go
+++ ctlog/ctlog.go
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 // Package ctlog contains data structures and methods to do with CT Log metadata
-// that is needed by the monitor.
+// that is needed by Monologue.
 //
 // TODO(katjoyce): Try to come up with a better package name.
 package ctlog
@@ -23,12 +23,10 @@ import (
 	"fmt"
 
 	ct "github.com/google/certificate-transparency-go"
-	"github.com/google/certificate-transparency-go/logid"
 )
 
-// Log contains metadata about a CT Log that is needed by the monitor.
+// Log contains metadata about a CT Log that is needed by Monologue.
 type Log struct {
-	ID        logid.LogID
 	Name      string
 	URL       string
 	PublicKey crypto.PublicKey
@@ -40,16 +38,11 @@ type Log struct {
 // takes log details from a log list struct based on the new Log list JSON
 // schema.
 func New(url, name, b64PubKey string) (*Log, error) {
-	id, err := logid.FromPubKeyB64(b64PubKey)
-	if err != nil {
-		return nil, fmt.Errorf("logid.FromPubKeyB64(): %s", err)
-	}
 	pk, err := ct.PublicKeyFromB64(b64PubKey)
 	if err != nil {
 		return nil, fmt.Errorf("ct.PublicKeyFromB64(): %s", err)
 	}
 	return &Log{
-		ID:        id,
 		Name:      name,
 		URL:       url,
 		PublicKey: pk,
