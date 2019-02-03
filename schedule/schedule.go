e3f30a59a3f312dc9037b3459da06a3f841424aa
Rob Percival <robpercival@google.com>
2019-02-03 21:13:34 +0000

    schedule.Every() now executes the given func immediately
    It will no longer wait for the specified period before making the first
    call - it will only wait between calls. Without this change, the collector
    will do nothing initially, because all getters will wait for their period
    to elapse before running.
    
    This makes startup faster, which is particularly helpful for testing.

--- schedule/schedule.go
+++ schedule/schedule.go
@@ -6,11 +6,13 @@ import (
 )
 
 // Every will call f periodically.
+// The first call will be made immediately.
 func Every(ctx context.Context, period time.Duration, f func(context.Context)) {
 	if ctx.Err() != nil {
 		return
 	}
-
+	// Run f immediately, then use a Ticker to periodically call it again.
+	f(ctx)
 	t := time.NewTicker(period)
 	defer t.Stop()
 	for {
