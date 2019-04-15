71bddc9aed8fe4e442a36a9abbd6c642aa6cffbb
David Drysdale <drysdale@google.com>
2019-04-15 15:36:18 +0100

    incident: incident reporting framework (#25)
    First pass on a simple incident reporting framework interface,
    with a glog-only implementation and a simple MySQL database table
    implementation (which can be shared between multiple tools).

---
+++ incident/mysql/incident.go
@@ -0,0 +1,55 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package mysql provides a MySQL based implementation of incident management.
+package mysql
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/golang/glog"
+	"github.com/google/monologue/incident"
+)
+
+type mysqlReporter struct {
+	db     *sql.DB
+	stmt   *sql.Stmt
+	source string
+}
+
+// NewMySQLReporter builds an incident.Reporter instance that records incidents
+// in a MySQL database, all of which will be marked as emanating from the given
+// source.
+func NewMySQLReporter(ctx context.Context, db *sql.DB, source string) (incident.Reporter, error) {
+	stmt, err := db.PrepareContext(ctx, "INSERT INTO Incidents(Source, BaseURL, Summary, Category, FullURL, Details) VALUES (?, ?, ?, ?, ?, ?);")
+	if err != nil {
+		return nil, fmt.Errorf("failed to prepare context for %q: %v", source, err)
+	}
+	return &mysqlReporter{db: db, source: source, stmt: stmt}, nil
+}
+
+// Log records an incident with the given details.
+func (m *mysqlReporter) Log(ctx context.Context, baseURL, summary, category, fullURL, details string) {
+	if _, err := m.stmt.ExecContext(ctx, m.source, baseURL, summary, category, fullURL, details); err != nil {
+		glog.Errorf("failed to insert incident for %q: %v", m.source, err)
+	}
+}
+
+// Logf records an incident with the given details and formatting.
+func (m *mysqlReporter) Logf(ctx context.Context, baseURL, summary, category, fullURL, detailsFmt string, args ...interface{}) {
+	details := fmt.Sprintf(detailsFmt, args...)
+	m.Log(ctx, baseURL, summary, category, fullURL, details)
+}
---
+++ incident/mysql/incident.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS Incidents(
+  Id SERIAL,
+  Source VARCHAR(128),
+  BaseURL VARCHAR(512),
+  Summary VARCHAR(2048),
+  Category VARCHAR(512),
+  FullURL VARCHAR(512),
+  Details TEXT,
+  -- OwningId indicates that an incident is considered a sub-incident of the owning incident.
+  OwningId BIGINT UNSIGNED NULL,
+  PRIMARY KEY(Id),
+  FOREIGN KEY(OwningId) REFERENCES Incidents(Id)
+);
+
+CREATE INDEX SourceIndex ON Incidents(Source);
+CREATE INDEX BaseURLIndex ON Incidents(BaseURL);
+CREATE INDEX SummaryIndex ON Incidents(Summary);
+CREATE INDEX FullURLIndex ON Incidents(FullURL);
+CREATE INDEX CategoryIndex ON Incidents(Category);
