be74dd736ad04f527103b2e4cfe4a8631da08d17
Kat Joyce <katjoyce@google.com>
2019-06-04 18:30:13 +0100

    Add License header.

--- collector/datacollector/datacollector.go
+++ collector/datacollector/datacollector.go
@@ -1,3 +1,17 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Datacollector runs a tool to collect the data needed to monitor Certificate
 // Transparency Logs, which can then be used to check that they are adhering to
 //RFC 6962.
